50805db34816f68739d4e78ba91c878d2d652a89
mingjue <zhening.czn@alibaba-inc.com>
2018-12-06 21:15:52 +0800

    upgrate RecyclerView to 28.0.0 version

---
+++ app/src/main/java/me/yifeiyuan/flapdev/DifferActivity.java
@@ -0,0 +1,13 @@
+package me.yifeiyuan.flapdev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DifferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_differ);
+    }
+}
---
+++ flap/src/main/java/me/yifeiyuan/flap/LayoutItemFactory.java
@@ -0,0 +1,50 @@
+package me.yifeiyuan.flap;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by 程序亦非猿
+ */
+public abstract class FlapItemFactory<T, VH extends FlapItem> implements ItemFactory<T> {
+
+    @SuppressWarnings("unchecked")
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull final LayoutInflater inflater, @NonNull final ViewGroup parent, final int viewType) {
+
+        View view = inflater.inflate(viewType, parent, false);
+
+        Class clazz = (Class<?>) ReflectUtils.getTypes(this)[1];
+
+        VH vh = null;
+        try {
+            Constructor constructor = clazz.getConstructor(View.class);
+            constructor.setAccessible(true);
+            vh = (VH) constructor.newInstance(view);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return vh;
+    }
+
+    @Override
+    public final int getItemViewType(final T model) {
+        return getLayoutResId(model);
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutResId(final T model);
+}

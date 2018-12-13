5fadbc3f7169d1d02bbadb5fda4b9ca857020d68
mingjue <zhening.czn@alibaba-inc.com>
2018-12-13 16:29:13 +0800

    support debugging setting & throw Exception when debugging

--- flap/src/main/java/me/yifeiyuan/flap/FlapDebug.java
+++ flap/src/main/java/me/yifeiyuan/flap/FlapDebug.java
@@ -1,11 +1,26 @@
 package me.yifeiyuan.flap;
 
+import android.util.Log;
+
 /**
  * Flap
  * Created by 程序亦非猿 on 2018/12/13.
  */
-public class FlapConfig {
+public final class FlapDebug {
 
-    public static boolean DEBUG = true;
+    private static final String TAG = "FlapDebug";
 
+    private static boolean DEBUG = false;
+
+    static void setDebug(boolean isDebugging) {
+        DEBUG = isDebugging;
+    }
+
+    static void throwIfDebugging(Exception e) {
+        if (DEBUG) {
+            throw new RuntimeException(e);
+        } else {
+            Log.e(TAG, "FlapDebug : ", e);
+        }
+    }
 }

0ab2d1534f6a222f97cbcaf828c9552c48f7c13f
Fitz <alancheen06@gmail.com>
2019-01-02 15:45:41 +0800

    create an delegate interface for Adapter

--- flap/src/main/java/me/yifeiyuan/flap/FlapAdapterDelegate.java
+++ flap/src/main/java/me/yifeiyuan/flap/FlapAdapterDelegate.java
@@ -4,14 +4,17 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 /**
  * Created by 程序亦非猿 on 2019/1/2.
  */
-interface AdapterDelegate {
+interface FlapAdapterDelegate {
 
     int getItemViewType(@NonNull Object model);
 
     @NonNull
     FlapItem onCreateViewHolder(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent, int viewType);
 
+    void onBindViewHolder(@NonNull final FlapItem holder, Object model, @NonNull FlapAdapter flapAdapter, @NonNull List<Object> payloads);
 }

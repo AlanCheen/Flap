09429747d3f5b8362d46659bd6652ec3c49423ae
Fitz <alancheen06@gmail.com>
2019-03-22 15:18:44 +0800

    add test ann stuff

--- app/src/main/java/me/yifeiyuan/flapdev/testann/AnnItem.java
+++ app/src/main/java/me/yifeiyuan/flapdev/testann/AnnItem.java
@@ -1,7 +1,50 @@
 package me.yifeiyuan.flapdev.testann;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+import me.yifeiyuan.flap.FlapAdapter;
+import me.yifeiyuan.flap.FlapItem;
+import me.yifeiyuan.flap.LayoutItemFactory;
+import me.yifeiyuan.flap.annotations.FlapItemFactory;
+import me.yifeiyuan.flapdev.R;
+
 /**
  * Created by 程序亦非猿 on 2019-03-22.
  */
-public class AnnItem {
+
+@FlapItemFactory(itemViewType = R.layout.flap_item_simple_text, modelClass = AnnModel.class)
+public class AnnItem extends FlapItem<AnnModel> {
+
+    private static final String TAG = "SimpleTextItem";
+
+    private TextView tvContent;
+
+    public AnnItem(final View itemView) {
+        super(itemView);
+//        tvContent = findViewById(R.id.tv_content);
+    }
+
+    @Override
+    protected void onBind(@NonNull final AnnModel model, @NonNull final FlapAdapter adapter, @NonNull final List<Object> payloads) {
+        Log.d(TAG, "onBind: " + getAdapterPosition());
+    }
+
+    public static class Factory extends LayoutItemFactory<AnnModel, AnnItem> {
+
+//        @Override
+//        protected int getLayoutResId(final AnnModel model) {
+//            return R.layout.flap_item_simple_text;
+//        }
+
+        @Override
+        protected int getLayoutResId() {
+            return R.layout.flap_item_simple_text;
+        }
+    }
+
 }

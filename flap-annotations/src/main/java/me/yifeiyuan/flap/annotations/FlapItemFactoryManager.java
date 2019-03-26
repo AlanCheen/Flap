683daa3dd918cf5f18002c1906aec671be547041
Fitz <alancheen06@gmail.com>
2019-03-26 16:13:56 +0800

    add annotation FlapItemFactoryManager , gen FlapItemFactoryManager for auto inject Factories

---
+++ flap-annotations/src/main/java/me/yifeiyuan/flap/annotations/FlapItemFactoryManager.java
@@ -0,0 +1,14 @@
+package me.yifeiyuan.flap.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by 程序亦非猿 on 2019-03-26.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface FlapItemFactoryManager {
+}

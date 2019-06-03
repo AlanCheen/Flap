9e6e590caa26623b55f7e35ece5a18f04f0971f4
Fitz <alancheen06@gmail.com>
2019-06-03 10:15:17 +0800

    add import code

--- flap/src/main/java/me/yifeiyuan/flap/internal/DefaultFlapItem.java
+++ flap/src/main/java/me/yifeiyuan/flap/internal/DefaultFlapItem.java
@@ -2,6 +2,7 @@ package me.yifeiyuan.flap.internal;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.RestrictTo;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

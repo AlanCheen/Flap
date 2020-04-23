dbbce0e6cd8893cc43a96c7c449567771223ee3c
Fitz <alancheen06@gmail.com>
2020-04-23 19:49:00 +0800

    add comment

--- flap-annotations/src/main/java/me/yifeiyuan/flap/annotations/Component.java
+++ flap-annotations/src/main/java/me/yifeiyuan/flap/annotations/Component.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 标记一个类为组件
+ * @author 程序亦非猿
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface Component {

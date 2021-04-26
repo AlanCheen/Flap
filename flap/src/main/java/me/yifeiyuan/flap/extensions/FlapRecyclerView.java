39e5d72a21aa14d9a0381053a1e1625fa93ab4bf
mingjue <zhening.czn@alibaba-inc.com>
2021-04-26 19:30:15 +0800

    更新 flaprecyclerview

--- flap/src/main/java/me/yifeiyuan/flap/extensions/FlapRecyclerView.java
+++ flap/src/main/java/me/yifeiyuan/flap/extensions/FlapRecyclerView.java
@@ -8,11 +8,13 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
+
 import me.yifeiyuan.flap.FlapAdapter;
+
 /**
  * 封装了 Flap 的 RecyclerView，未测试，暂时请不要使用。
  * todo
- *
+ * <p>
  * Flap Github: <a>https://github.com/AlanCheen/Flap</a>
  *
  * @author 程序亦非猿 [Follow me](<a> https://github.com/AlanCheen</a>)
@@ -25,20 +27,20 @@ public class FlapRecyclerView extends RecyclerView {
 
     public FlapRecyclerView(@NonNull final Context context) {
         super(context);
-        init();
+        init(context, null, 0);
     }
 
     public FlapRecyclerView(@NonNull final Context context, @Nullable final AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context, attrs, 0);
     }
 
     public FlapRecyclerView(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyle) {
         super(context, attrs, defStyle);
-        init();
+        init(context, attrs, defStyle);
     }
 
-    private void init() {
+    private void init(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyle) {
         adapter = new FlapAdapter();
         setAdapter(adapter);
     }

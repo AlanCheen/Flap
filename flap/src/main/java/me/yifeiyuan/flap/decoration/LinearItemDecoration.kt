2b1a63606870c424aa3a8f2380e41dd8be10db7c
Fitz <zhening.czn@alibaba-inc.com>
2022-08-19 15:44:57 +0800

    增加适用于 LLM 的 ItemDecorations

--- flap/src/main/java/me/yifeiyuan/flap/decoration/LinearItemDecoration.kt
+++ flap/src/main/java/me/yifeiyuan/flap/decoration/LinearItemDecoration.kt
@@ -1,12 +1,21 @@
-package me.yifeiyuan.flapdev
+package me.yifeiyuan.flap.decoration
 
 import android.content.Context
-import android.graphics.drawable.ColorDrawable
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 
 /**
+ * 可以绘制 Drawable 的 ItemDecoration，可以设置间隔的颜色和大小
+ *
+ * 如果不需要绘制任何颜色，可以使用：
+ * @see LinearSpaceItemDecoration
+ *
  * Created by 程序亦非猿 on 2022/8/18.
+ *
+ * @since 3.0.4
  */
 class LinearItemDecoration : DividerItemDecoration {
 
@@ -16,25 +25,20 @@ class LinearItemDecoration : DividerItemDecoration {
         }
     }
 
-    constructor(context: Context, orientation: Int = VERTICAL) : super(context, orientation)
-
     /**
-     * @param color 颜色 , Color.parseColor("#ff0000")
+     * @param color 颜色 ,默认透明色 Color.parseColor("#00000000")
      * @param sizeInPx 尺寸，单位是像素
      */
-    constructor(context: Context, color: Int, sizeInPx: Int, orientation: Int = VERTICAL) : super(context, orientation) {
-        val colorDrawable = SizeColorDrawable(color, sizeInPx)
+    constructor(context: Context, sizeInPx: Int, color: Int = Color.parseColor("#00000000"), orientation: Int = VERTICAL) : super(context, orientation) {
+        val colorDrawable = SizedColorDrawable(color, sizeInPx)
         setDrawable(colorDrawable)
     }
-}
 
-class SizeColorDrawable(color: Int, private val size: Int) : ColorDrawable(color) {
-
-    override fun getIntrinsicHeight(): Int {
-        return size
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
     }
 
-    override fun getIntrinsicWidth(): Int {
-        return size
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
     }
 }
--- flap/src/main/java/me/yifeiyuan/flap/decoration/LinearSpaceItemDecoration.kt
+++ flap/src/main/java/me/yifeiyuan/flap/decoration/LinearSpaceItemDecoration.kt
@@ -1,7 +1,24 @@
 package me.yifeiyuan.flap.decoration
 
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 /**
+ * 只绘制间隔的 ItemDecoration，适用于 LinearLayoutManager
+ *
+ * @param space 间隔，单位像素
+ * @param orientation 方向，默认垂直
+ *
  * Created by 程序亦非猿 on 2022/8/19.
+ * @since 3.0.4
  */
-class LinearSpaceItemDecoration {
+class LinearSpaceItemDecoration(var space: Int, var orientation: Int = RecyclerView.VERTICAL) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        if (orientation == RecyclerView.VERTICAL) {
+            outRect.set(0, 0, 0, space)
+        } else {
+            outRect.set(0, 0, space, 0)
+        }
+    }
 }

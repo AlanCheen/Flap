040fe70c8ad267f2b1d69bf23d3b2d4ec7bf8e91
Fitz <zhening.czn@alibaba-inc.com>
2022-09-08 20:20:21 +0800

    t

---
+++ flap/src/main/java/me/yifeiyuan/flap/FlapManager.kt
@@ -0,0 +1,11 @@
+package me.yifeiyuan.flap
+
+import me.yifeiyuan.flap.delegate.IAdapterDelegateManager
+import me.yifeiyuan.flap.hook.IAdapterHookManager
+import me.yifeiyuan.flap.service.IAdapterServiceManager
+
+/**
+ * Created by 程序亦非猿 on 2022/9/8.
+ */
+class FlapManager :IAdapterServiceManager,IAdapterDelegateManager,IAdapterHookManager{
+}

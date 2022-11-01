8741a0dfd9f239e4e8f21d70932a6348757a6219
Fitz <alancheen06@gmail.com>
2022-11-01 19:20:48 +0800

    catch onScrollStateChanged 规避系统 StaggeredGridLayoutManager 问题

--- flap/src/main/java/me/yifeiyuan/flap/widget/FlapIndexedStaggeredGridLayoutManager.java
+++ flap/src/main/java/me/yifeiyuan/flap/widget/FlapIndexedStaggeredGridLayoutManager.java
@@ -316,11 +316,15 @@ public class FlapIndexedStaggeredGridLayoutManager extends RecyclerView.LayoutMa
 
     @Override
     public void onScrollStateChanged(int state) {
-        if (state == RecyclerView.SCROLL_STATE_IDLE) {
-            checkForGaps();
-            if (reLayoutOnScrollToTop) {
-                handleAutoReLayoutWhenScrollToTop();
+        try {
+            if (state == RecyclerView.SCROLL_STATE_IDLE) {
+                checkForGaps();
+                if (reLayoutOnScrollToTop) {
+                    handleAutoReLayoutWhenScrollToTop();
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
